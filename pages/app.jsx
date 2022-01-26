b33b5ef0b3a0a74893d86c5fedd4a22252f91c8a
Vivek <audit536@gmail.com>
2022-01-26 15:38:04 +0530

    Revert "Add hotjar analytics"
    This reverts commit 544ce94441cf15d7e6ffe12e89d8d756fa6a4856.

--- pages/app.jsx
+++ pages/app.jsx
@@ -285,16 +285,6 @@ export default function Home() {
       setFetching(true)
       fetchArticle(urlParam, setFetching, setHugeText)
     }
-
-    // Hotjar Tracking Code for https://start-listening.herokuapp.com/
-    (function(h,o,t,j,a,r){
-      h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:2800643,hjsv:6};
-      a=o.getElementsByTagName('head')[0];
-      r=o.createElement('script');r.async=1;
-      r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-      a.appendChild(r);
-  })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
   },[]);
 
   useEffect(()=>{
