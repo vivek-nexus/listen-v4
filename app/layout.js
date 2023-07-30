dd3c377cdb28f967547ba0bc70c6ab4f513dd68e
Vivek G <audit536@gmail.com>
2023-07-30 15:27:43 +0530

    Remove Next.js defaults and set up defaults

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Cabin } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const cabin = Cabin({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -11,7 +11,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cabin.className}>
+        {children}</body>
     </html>
   )
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    env: {
+        // Sample LINK prefix: /project-pratima
+        // Do not add the slash at the last
+        LINK_PREFIX: isProd ? "/project-pratima" : ""
+    },
+    basePath: isProd ? '/listen' : '',
+    assetPrefix: isProd ? '/listen' : '',
+}
 
 module.exports = nextConfig
