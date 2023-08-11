7defa6a99dd1dc230fb23943e1b2bb62f8c486ad
Vivek G <audit536@gmail.com>
2023-08-11 20:36:35 +0530

    Add mouseflow analytics and update description

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,11 @@
+"use client"
+
 import "./globals.css"
 import 'material-icons/iconfont/material-icons.css';
 import { Cabin } from "next/font/google"
 import "animate.css";
 import Head from "@/components/Head";
+import { useEffect } from "react";
 
 const cabin = Cabin({ subsets: ["latin"] })
 
@@ -34,6 +37,16 @@ const cabin = Cabin({ subsets: ["latin"] })
 // }
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    window._mfq = window._mfq || [];
+    (function () {
+      var mf = document.createElement("script");
+      mf.type = "text/javascript"; mf.defer = true;
+      mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+      document.getElementsByTagName("head")[0].appendChild(mf);
+    })();
+  })
+
   return (
     <html lang="en">
       <Head />
--- components/Head.jsx
+++ components/Head.jsx
@@ -4,7 +4,7 @@ export default function Head() {
     return (
         <head>
             <title>Listen</title>
-            <meta name="description" content="Your own podcast buddy!" />
+            <meta name="description" content="Stop long form reading, cut screen time and start listening!" />
             <meta name="viewport" content="width=device-width, initial-scale=1.0" />
             <link rel="icon" href={`${env.LINK_PREFIX}/logo.png`} />
             {/* <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@48,400,1,0" /> */}
@@ -16,7 +16,7 @@ export default function Head() {
             <meta property="og:title" content="Listen" />
             <meta property="og:url" content="https://yakshag.github.io/listen/" />
             <meta property="og:image" content="https://yakshag.github.io/listen/link-preview.png" />
-            <meta property="og:description" content="Your own podcast buddy!" />
+            <meta property="og:description" content="Stop long form reading, cut screen time and start listening!" />
         </head>
     )
 }
