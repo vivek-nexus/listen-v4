b30c37307a54ce61038b1a6347d455f315c86ae2
yakshaG <audit536@gmail.com>
2024-02-03 13:46:17 +0530

    Disable mouseflow

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,15 +16,15 @@ const lexend = Lexend({ subsets: ["latin"] })
 
 
 export default function RootLayout({ children }) {
-  useEffect(() => {
-    window._mfq = window._mfq || [];
-    (function () {
-      var mf = document.createElement("script");
-      mf.type = "text/javascript"; mf.defer = true;
-      mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
-      document.getElementsByTagName("head")[0].appendChild(mf);
-    })();
-  }, [])
+  // useEffect(() => {
+  //   window._mfq = window._mfq || [];
+  //   (function () {
+  //     var mf = document.createElement("script");
+  //     mf.type = "text/javascript"; mf.defer = true;
+  //     mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+  //     document.getElementsByTagName("head")[0].appendChild(mf);
+  //   })();
+  // }, [])
 
   return (
     <html lang="en">
