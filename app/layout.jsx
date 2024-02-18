4f0a84e5bbada2bce7d979a7b98f4a09adeb0c82
yakshaG <audit536@gmail.com>
2024-02-18 11:07:11 +0530

    Fix mouseflow enable logic

--- app/layout.jsx
+++ app/layout.jsx
@@ -20,18 +20,15 @@ export default function RootLayout({ children }) {
   useEffect(() => {
     const searchParams = new URLSearchParams(window.location.search);
     const paramURLValue = searchParams.get("url")
-    const paramTextValue = searchParams.get("text")
 
-    if ((paramURLValue !== null)) {
-      if (!paramURLValue.includes("corrieredellacalabria.it")) {
-        window._mfq = window._mfq || [];
-        (function () {
-          var mf = document.createElement("script");
-          mf.type = "text/javascript"; mf.defer = true;
-          mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
-          document.getElementsByTagName("head")[0].appendChild(mf);
-        })();
-      }
+    if (!paramURLValue || !paramURLValue.includes("corrieredellacalabria.it")) {
+      window._mfq = window._mfq || [];
+      (function () {
+        var mf = document.createElement("script");
+        mf.type = "text/javascript"; mf.defer = true;
+        mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+        document.getElementsByTagName("head")[0].appendChild(mf);
+      })();
     }
   }, [])
 
