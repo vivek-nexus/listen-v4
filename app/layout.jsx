a8bd3afde3eaefa64dd20df7758f9a9ea455ea9c
yakshaG <audit536@gmail.com>
2024-02-18 11:01:54 +0530

    Renable mouseflow

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,15 +16,24 @@ const lexend = Lexend({ subsets: ["latin"] })
 
 
 export default function RootLayout({ children }) {
-  // useEffect(() => {
-  //   window._mfq = window._mfq || [];
-  //   (function () {
-  //     var mf = document.createElement("script");
-  //     mf.type = "text/javascript"; mf.defer = true;
-  //     mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
-  //     document.getElementsByTagName("head")[0].appendChild(mf);
-  //   })();
-  // }, [])
+
+  useEffect(() => {
+    const searchParams = new URLSearchParams(window.location.search);
+    const paramURLValue = searchParams.get("url")
+    const paramTextValue = searchParams.get("text")
+
+    if ((paramURLValue !== null)) {
+      if (!paramURLValue.includes("corrieredellacalabria.it")) {
+        window._mfq = window._mfq || [];
+        (function () {
+          var mf = document.createElement("script");
+          mf.type = "text/javascript"; mf.defer = true;
+          mf.src = "//cdn.mouseflow.com/projects/d650c614-2064-44cc-804a-54644c37dd52.js";
+          document.getElementsByTagName("head")[0].appendChild(mf);
+        })();
+      }
+    }
+  }, [])
 
   return (
     <html lang="en">
