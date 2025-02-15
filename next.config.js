4e1ac66f0b6b97c82f1857a086c6177ee10f6008
Vivek G <audit536@gmail.com>
2025-02-15 20:52:37 +0530

    Fix link prefix mistake

--- next.config.js
+++ next.config.js
@@ -7,10 +7,10 @@ const nextConfig = {
     env: {
         // Sample LINK prefix: /listen
         // Do not add the slash at the last
-        LINK_PREFIX: isProd ? "listen-v4" : ""
+        LINK_PREFIX: isProd ? "/listen-v4" : ""
     },
-    basePath: isProd ? 'listen-v4' : '',
-    assetPrefix: isProd ? 'listen-v4' : '',
+    basePath: isProd ? '/listen-v4' : '',
+    assetPrefix: isProd ? '/listen-v4' : '',
 }
 
 module.exports = nextConfig
